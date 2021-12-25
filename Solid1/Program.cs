a3a0dbd46b430e4a30f4afc1f20111bafaefe76d
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-12-25 10:09:55 +0200

    Add project files.

---
+++ Solid1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid1
+{
+    //Який принцип S.O.L.I.D. порушено? Виправте!
+    // Порушено Single Responsbility Principle
+    class Item
+    {
+        public void AddItem() {/*...*/}
+        public void DeleteItem() {/*...*/}
+        public void GetItems() {/*...*/}
+        public void GetItemCount() {/*...*/}
+    }
+    class DisplayOrder
+    {
+        public void PrintOrder() {/*...*/}
+        public void ShowOrder() {/*...*/}
+
+    }
+    class OrdersDB
+    {
+        public void Load() {/*...*/}
+        public void Save() {/*...*/}
+    }
+    class Order
+    {
+        public void CalculateTotalSum() {/*...*/}
+        public void Update() {/*...*/}
+        public void Delete() {/*...*/}
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ Solid_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+abstract class Figure
+{
+    public abstract int GetArea();
+}
+class Rectangle : Figure
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public override int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!! Тепер створено інтерфейс!
+class Square : Figure
+{
+    public int Width { get; set; }
+
+    public override int GetArea()
+    {
+        return Width * Width;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Figure rect = new Rectangle { Width = 5, Height = 10 };
+        Console.WriteLine(rect.GetArea());
+        //Відповідь 100? Що не так??? Було порушено принцип підстановки Барбари Лісков.
+        Console.ReadKey();
+    }
+}
+
